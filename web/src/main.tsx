bba7c8f82a91788173cc89f43f52867d467622e2
Yury Saukou <yury_saukou@epam.com>
2025-03-02 18:32:10 +0400

    add tonconnect base manifest

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,4 +1,5 @@
 import WebApp from '@twa-dev/sdk'
+import eruda from 'eruda'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
@@ -7,6 +8,10 @@ import App from './App.tsx'
 import './index.css'
 
 WebApp.ready();
+
+if (import.meta.env.DEV) {
+  eruda.init();
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
